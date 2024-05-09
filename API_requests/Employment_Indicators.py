d8c7bc4276718a8f7205553faa65ee827dd39c53
Ruben Castaing <castaingruben@gmail.com>
2024-05-10 10:40:53 +1200

    Got Employment Indicators working

---
+++ API_requests/Employment_Indicators.py
@@ -0,0 +1,25 @@
+import urllib.request, json
+import stats_odata as odata
+import pandas as pd
+from datetime import datetime, timedelta
+
+def get_employment_indicators():
+    # Calculate the date for two months before today
+    two_months_ago = (datetime.now() - timedelta(days=60)).strftime('%Y-%m-%d')
+
+    endpoint = 'EmploymentIndicators'
+    service = "https://api.stats.govt.nz/odata/v1"
+    entity = "Observations" 
+    query_option = f"$filter=Period ge {two_months_ago}"
+    api_key = "dde082d59e0a4c49bf83f67ff6b9b032"
+    proxies = {}
+
+    #Sending an API request
+    Observations = odata.get_odata(service, endpoint, entity, query_option, api_key, proxies)
+
+    #Cleaning the data
+    columns_to_drop = ['id', 'ResourceID', 'Status', 'Label3', 'Label4', 'Label5', 'Label6']
+    #all_data_from_month.drop(columns_to_drop, axis=1, inplace=True)
+    Observations.drop(columns_to_drop, axis=1, inplace=True)
+
+    return(Observations)
