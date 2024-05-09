13a9e49cf6daee42b69ec9e08c0bc21d9091c7b7
Ruben Castaing <castaingruben@gmail.com>
2024-05-10 10:00:39 +1200

    Deleting stats nz old files

--- accessing_the_catalog.py
+++ accessing_the_catalog.py
@@ -1,22 +1,34 @@
 import urllib.request, json
-import stats_odata as odata
+import pandas as pd
+
+api_key = "dde082d59e0a4c49bf83f67ff6b9b032"
 
 try:
     url = "https://api.stats.govt.nz/opendata/v1/data.json"
     
-    Observations = odata.get_odata(url, proxies)
-    print(Observations)
-    
-    hdr ={
-    # Request headers
-    'Cache-Control': 'no-cache',
+    hdr = {
+        'Cache-Control': 'no-cache',
+        'Ocp-Apim-Subscription-Key': api_key, 
+        'user-agent': ''
     }
 
     req = urllib.request.Request(url, headers=hdr)
-
     req.get_method = lambda: 'GET'
-    response = urllib.request.urlopen(req)
-    print(response.getcode())
-    print(response.read())
+    with urllib.request.urlopen(req) as response:
+        # Read the response and decode it
+        response_data = response.read().decode('utf-8')
+
+        # Load the JSON into a Python dictionary
+        data = json.loads(response_data)
+
+        # Use json_normalize to convert the nested JSON to a flat table
+        df = pd.json_normalize(data, record_path=['dataset'])
+
+        # Display the DataFrame
+        pd.set_option('display.max_rows', 100)
+        print(df)
+        df.to_csv('stats_nz_data.csv', index=False)
+
+
 except Exception as e:
     print(e)
--- another_go.py
+++ another_go.py
@@ -1,13 +1,17 @@
 import urllib.request, json
 import stats_odata as odata
 
+List_of_endpoints = ['EmploymentIndicators', 'OverseasCargo', 'Covid-19Indicators', 'InternationalMigration', 'HouseholdLabourForceSurvey', '2018Census-PopulationDwellings', 'OverseasMerchandiseTrade', 'InternationalTravel', 'NationalAccounts']
+Dictionary_of_columns = {Employment_Indicators: }
+
 service = "https://api.stats.govt.nz/odata/v1"
-endpoint = "EmploymentIndicators"
 entity = "Observations" 
-query_option = "$select=ResourceID,Period,Duration,Label1,Label2,Value,Unit,Measure,Multiplier&$top=10"
+query_option = "$top=10"
 api_key = "dde082d59e0a4c49bf83f67ff6b9b032"
 proxies = {}
 
-#Observations = odata.get_odata(service, endpoint, entity, query_option, api_key, proxies)
-#print(Observations)
-
+for endpoint in List_of_endpoints:
+    Observations = odata.get_odata(service, endpoint, entity, query_option, api_key, proxies
+    print(Observations)
+    output_string = endpoint + '.csv'
+    Observations.to_csv(output_string, index=False)
